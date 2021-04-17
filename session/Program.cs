d58e0ae7ce39a34642d0f595523c89432a8df740
Jordy Hamwijk <59865566+darksos34@users.noreply.github.com>
2021-04-17 15:26:21 +0200

    C# Part 1

---
+++ session/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace session
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string songLyrics = "You say greetings, and I say hello";
+
+            var result = songLyrics.Contains("goodbey");
+            Console.WriteLine(result);
+
+            Console.WriteLine(songLyrics.Contains("greetings"));
+        }
+    }
+}
